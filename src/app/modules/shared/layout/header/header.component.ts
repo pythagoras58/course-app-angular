4556319d45fb08a74b894c2bfa92d70ed1387674
pythagoras58 <solomondanso58@gmail.com>
2022-12-11 21:53:03 +0000

    Directive 1

--- src/app/modules/shared/layout/header/header.component.ts
+++ src/app/modules/shared/layout/header/header.component.ts
@@ -7,6 +7,9 @@ import {Component, OnInit} from "@angular/core";
 })
 
 export class HeaderComponent implements OnInit{
+
+  constructor() {
+  }
   ngOnInit(): void {
     console.log('NG HEADER')
   }
