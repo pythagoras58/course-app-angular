1b72359d338efa51c18d3c9bbdbb86c156e22269
pythagoras58 <solomondanso58@gmail.com>
2022-12-11 19:44:19 +0000

    Components

--- src/app/modules/shared/layout/header/header.component.ts
+++ src/app/modules/shared/layout/header/header.component.ts
@@ -2,8 +2,8 @@ import {Component} from "@angular/core";
 
 @Component({
   selector: 'app-header',
-  template : '',
-  styles:['']
+  templateUrl : 'header.component.html',
+  styleUrls:['header.component.css']
 })
 
 export class HeaderComponent{}
