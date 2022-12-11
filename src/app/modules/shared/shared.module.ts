3ae2178c327726a7e952a21ac3493f4927d7602a
pythagoras58 <solomondanso58@gmail.com>
2022-12-11 19:50:59 +0000

    Components - Footer

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,12 +1,15 @@
 import {NgModule} from "@angular/core";
 import {HeaderComponent} from "./layout/header/header.component";
+import { FooterComponent } from './layout/footer/footer.component';
 
 @NgModule({
   declarations: [
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   exports :[
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ]
 })
 
