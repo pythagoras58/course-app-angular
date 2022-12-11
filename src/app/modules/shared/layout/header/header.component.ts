fa77372f8ffceda8cac77d6ab9d8b77981ca97fb
pythagoras58 <solomondanso58@gmail.com>
2022-12-11 21:43:26 +0000

    Components - Footer

--- src/app/modules/shared/layout/header/header.component.ts
+++ src/app/modules/shared/layout/header/header.component.ts
@@ -1,4 +1,4 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
 
 @Component({
   selector: 'app-header',
@@ -6,4 +6,8 @@ import {Component} from "@angular/core";
   styleUrls:['header.component.css']
 })
 
-export class HeaderComponent{}
+export class HeaderComponent implements OnInit{
+  ngOnInit(): void {
+    console.log('NG HEADER')
+  }
+}
