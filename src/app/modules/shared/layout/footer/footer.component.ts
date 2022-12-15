f1fe86da3f44648eb28cfdf151b7f27330b369e8
pythagoras58 <solomondanso58@gmail.com>
2022-12-15 21:47:53 +0000

    Right class as DI 2

--- src/app/modules/shared/layout/footer/footer.component.ts
+++ src/app/modules/shared/layout/footer/footer.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {VersionService} from "../../../../services/version.service";
+import {RightService} from "../../../../services/right.service";
 
 @Component({
   selector: 'app-footer',
@@ -11,10 +12,12 @@ export class FooterComponent implements OnInit {
   array = [1,2,3,4,5];
 
   version : string | undefined;
-  constructor(private versionService : VersionService) { }
+  copyright : string | undefined;
+  constructor(private versionService : VersionService, private rightService : RightService) { }
 
   ngOnInit(): void {
     this.version = this.versionService.getVersion()
+    this.copyright = this.rightService.getCopyright();
     console.log('NG FOOTER')
   }
 
