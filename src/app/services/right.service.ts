a5946f8300e61e95406b9aefc853ea5959545c5f
pythagoras58 <solomondanso58@gmail.com>
2022-12-15 21:45:10 +0000

    Right class as DI

--- src/app/services/right.service.ts
+++ src/app/services/right.service.ts
@@ -3,5 +3,9 @@ import {Injectable} from "@angular/core";
 @Injectable({providedIn : 'root'})
 
 export class RightService{
+    private copyright = 'Pythagoras';
 
+    public getCopyright(){
+      return this.copyright;
+    }
 }
