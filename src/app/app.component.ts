a3e2dafd0bb0a7ad1eba12c6e26aae8dc92975ba
pythagoras58 <solomondanso58@gmail.com>
2022-12-21 17:08:48 +0000

    RX 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {Observable, of} from "rxjs";
 
 @Component({
   selector: 'app-root',
@@ -19,16 +20,22 @@ export class AppComponent implements  OnInit{
   constructor(private formBuilder : FormBuilder) {
   }
 
-
+  observableS : Observable<number> = of(1,2,3,4,5);
 
 
   ngOnInit(): void {
+    this.displayObservableNumbers();
     this.formGroup = this.formBuilder.group({
       name : [undefined, [Validators.required, Validators.minLength(8)]],
       surname : [undefined, [Validators.required,Validators.minLength(8)]]
     });
   }
 
+  private displayObservableNumbers(): void{
+      this.observableS.subscribe(value => {
+        console.log(value);
+      });
+  }
   onFormSubmit() : void{
     if(this.formGroup?.valid === false){
       console.log('Null Submission')
