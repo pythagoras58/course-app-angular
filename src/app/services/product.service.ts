95d09ebcab3786ef6d5ca6e67672ebacbdca8abc
pythagoras58 <solomondanso58@gmail.com>
2022-12-23 23:57:30 +0000

    API communication - DI product service into product component

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,5 +1,8 @@
 import {Injectable} from "@angular/core";
 import {environment} from "../../environments/environment";
+import {Observable} from "rxjs";
+import {ProductModel} from "../models/product.model";
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn : 'root'
@@ -7,4 +10,11 @@ import {environment} from "../../environments/environment";
 export class ProductService{
   // define the api
   private readonly apiUrl = environment.apiUrl;
+
+  constructor(private http : HttpClient) {
+  }
+
+  getProduct() : Observable<ProductModel[]>{
+    return this.http.get<ProductModel[]>(this.apiUrl+'/products');
+  }
 }
