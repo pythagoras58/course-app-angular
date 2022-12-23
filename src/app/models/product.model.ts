6ef45ffafe0065244203aa0ba54f12a455b7fe35
pythagoras58 <solomondanso58@gmail.com>
2022-12-23 23:52:25 +0000

    API communication - product model

---
+++ src/app/models/product.model.ts
@@ -0,0 +1,8 @@
+
+export interface ProductModel{
+  id : number;
+  title : string;
+  description : string;
+  price : number;
+  rating : number;
+}
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,6 +1,10 @@
 import {Injectable} from "@angular/core";
+import {environment} from "../../environments/environment";
 
 @Injectable({
   providedIn : 'root'
 })
-export class ProductService{}
+export class ProductService{
+  // define the api
+  private readonly apiUrl = environment.apiUrl;
+}
