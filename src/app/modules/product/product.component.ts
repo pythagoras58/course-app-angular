7bafc53446e05c9fa516c2b24465a324bac9983d
pythagoras58 <solomondanso58@gmail.com>
2022-12-24 00:01:36 +0000

    API communication - OnDestroy

--- src/app/modules/product/product.component.ts
+++ src/app/modules/product/product.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {ProductService} from "../../services/product.service";
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.css']
 })
-export class ProductComponent implements OnInit {
+export class ProductComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+
+  constructor(private productService : ProductService) { }
 
   ngOnInit(): void {
+    this.productService.getProduct().subscribe();
+
+  }
+
+  ngOnDestroy(): void {
   }
 
 }
